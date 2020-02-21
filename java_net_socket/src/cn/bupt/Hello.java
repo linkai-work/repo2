dab87cf59fba4c464f736ce230d82b37747e32ad
linkai <linkai_work@163.com>
2020-02-21 21:20:47 +0800

    first commit

---
+++ java_net_socket/src/cn/bupt/Hello.java
@@ -0,0 +1,7 @@
+package cn.bupt;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ java_net_socket/src/cn/bupt/collections/Demo1.java
@@ -0,0 +1,14 @@
+package cn.bupt.collections;
+
+public class Demo1{
+    public static void main(String[] args){
+        String s1="apple";
+        String s2="apple";
+        System.out.println(s1==s2);    //true
+        System.out.println(s1.equals(s2));    //equals比较的是内容，true
+        String s3=new String("apple");
+        String s4=new String("apple");
+        System.out.println(s3==s4);  //false
+        System.out.println(s3.  equals(s4));  //true
+    }
+}

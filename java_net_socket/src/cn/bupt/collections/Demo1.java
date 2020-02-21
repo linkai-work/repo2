404e487eee7e9fc604757e6501510d98a7818e2f
linkai <linkai_work@163.com>
2020-02-21 21:28:03 +0800

    first commit

--- java_net_socket/src/cn/bupt/collections/Demo1.java
+++ java_net_socket/src/cn/bupt/collections/Demo1.java
@@ -10,5 +10,6 @@ public class Demo1{
         String s4=new String("apple");
         System.out.println(s3==s4);  //false
         System.out.println(s3.  equals(s4));  //true
+        System.out.println(232);
     }
 }
